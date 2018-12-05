a1e670c85c81449fd7e598659ba0ae3054e9971e
Spinaker1 <klitschko456@gmail.com>
2018-12-05 21:05:55 +0100

    Poprawa zgodnie z zaleceniami mgr-a Parewicza

--- file.pl
+++ file.pl
@@ -1,10 +1,18 @@
-goal_achieved(on(X,Y), State) :- member(on(X,Y), State).
-goal_achieved(clear(X), State) :- member(clear(X), State).
-goal_achieved(clear(X/on(X,Y)), State) :- goal_achieved(on(X,Y), State), goal_achieved(clear(X), State).
-goal_achieved(on(X,Y/on(Y,Z)), State) :- goal_achieved(on(Y,Z), State), goal_achieved(on(X,Y), State).
-goal_achieved(diff(Z,X/on(X,Y)), State) :- goal_achieved(on(X,Y), State), dif(Z,X).
+member1(X,[X|_]).
+member1(X,[_|Rest]) :- member1(X,Rest).
 
-plan(State, Goals, [], State) :- goals_achieved(Goals, State).
+substract1(_,[],[]).
+substract1(X,[X|Rest],Rest).
+substract1(X,[Y|Rest],[Y|Result]) :- substract1(X,Rest,Result).
+
+substract_all([],OldList,OldList).
+substract_all([X|Rest],OldList,NewList) :- substract1(X,OldList,Temp),substract_all(Rest,Temp,NewList).
+
+goal_achieved(on(X,Y), State) :- \+ compound(Y) , member1(on(X,Y), State).
+goal_achieved(clear(X), State) :- \+ compound(X) , member1(clear(X), State).
+goal_achieved(clear(X/on(X,Y)), State) :- goal_achieved(on(X,Y), State), member1(clear(X), State).
+goal_achieved(on(X,Y/on(Y,Z)), State) :- goal_achieved(on(Y,Z), State), member1(on(X,Y), State).
+goal_achieved(diff(Z,X/on(X,Y)), State) :- goal_achieved(on(X,Y), State), Z \= X.
 
 %jeśli nie ma żadnych celów, to znaczy, że są spełnione
 goals_achieved([], _). 
@@ -12,21 +20,26 @@ goals_achieved([], _).
 goals_achieved([X|Goals], State) :- goals_achieved(Goals, State), goal_achieved(X,State), \+member(X,Goals). 
 
 %wybieramy element który należy listy celów, ale nie należy do stanu początkowego
-choose_goal(Goal, Goals, RestGoals,InitState) :- select(Goal,Goals,RestGoals), \+member(Goal,InitState). 
+choose_goal(Goal, Goals, RestGoals,InitState) :- select(Goal,Goals,RestGoals), \+goal_achieved(Goal,InitState). 
 
 achieves(on(X,Z),move(X,Y/on(X,Y),Z)). 
-achieves(clear(Y),move(X/on(X,Y),Y,Z)). 
+achieves(clear(Y),move(X/on(X,Y),Y,_)). 
 
 requires(move(X,Y/on(X,Y),Z),[clear(X),clear(Z)],[on(X,Y)]).
-requires(move(X/on(X,Y),Y,Z),[clear(X/on(X,Y))],[clear(Z),diff(Z,X/on(X,Y))]).
+requires(move(X/on(X,Y),Y,Z),[clear(X/on(X,Y))],[diff(Z,X/on(X,Y)), clear(Z)]).
 
-inst_action(move(X/on(_,_),Y/on(_,_),Z),Cond,State,move(X,Y,Z)) :- goals_achieved(Cond,State).
-inst_action(move(X/on(_,_),Y,Z),Cond,State,move(X,Y,Z)) :- goals_achieved(Cond,State).
-inst_action(move(X,Y/on(_,_),Z),Cond,State,move(X,Y,Z)) :- goals_achieved(Cond,State).
+inst_action(Action, Conditions, State1, InstAction) :- dupa(Action,InstAction),goals_achieved(Conditions,State1).
 
-perform_action(State1,move(X,Y,Z),State2) :-  append(State1,[clear(Y),on(X,Z)],Temp),subtract(Temp,[clear(Z),on(X,Y)],State2).
+dupa(move(X/on(_,_),Y/on(_,_),Z),move(X,Y,Z)).
+dupa(move(X/on(_,_),Y,Z),move(X,Y,Z)).
+dupa(move(X,Y/on(_,_),Z),move(X,Y,Z)).
 
-conc(L1,L2,L3) :- append(L1,L2,L3).
+perform_action(State1,move(X,Y,Z),[clear(Y),on(X,Z) | State2]) :-  substract_all([clear(Z),on(X,Y)],State1,State2).
+
+conc([],L2,L2).
+conc([X|Rest1],L2,[X|Rest2]) :- conc(Rest1,L2,Rest2).
+
+plan(State, Goals, [], State) :- goals_achieved(Goals, State).
 
 plan(InitState, Goals, Plan, FinalState) :-
 choose_goal(Goal, Goals, RestGoals, InitState),
