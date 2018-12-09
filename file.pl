e9e68f57a57f93a4118a82376c046c4cb93df5ca
Spinaker1 <klitschko456@gmail.com>
2018-12-09 17:54:37 +0100

    Commit

--- file.pl
+++ file.pl
@@ -14,12 +14,13 @@ substract1(X,[Y|Rest],[Y|Result]) :- substract1(X,Rest,Result).
 substract_all([],OldList,OldList).
 substract_all([X|Rest],OldList,NewList) :- substract1(X,OldList,Temp),substract_all(Rest,Temp,NewList).
 
-%Dla celów, gdzie X i Y są już ukonkretnione (bez warunków), sprawdzamy czy ni są strukturą (compound)
+%Dla celów, gdzie X i Y są już ukonkretnione (bez warunków), sprawdzamy czy nie są strukturą (compound)
 goal_achieved(on(X,Y), State) :- \+ compound(Y) , member1(on(X,Y), State).
 goal_achieved(clear(X), State) :- \+ compound(X) , member1(clear(X), State).
 goal_achieved(clear(X/on(X,Y)), State) :- goal_achieved(on(X,Y), State), member1(clear(X), State).
-goal_achieved(on(X,Y/on(Y,Z)), State) :- goal_achieved(on(Y,Z), State), member1(on(X,Y), State).
-goal_achieved(diff(Z,X/on(X,Y)), State) :- goal_achieved(on(X,Y), State), Z \= X.
+%Zmienna Z, czyli miejsce, gdzie przenosimy, nie może mieć warunków
+goal_achieved(on(X,Y/on(Y,Z)), State) :- \+ compound(Z), goal_achieved(on(Y,Z), State), member1(on(X,Y), State).
+goal_achieved(diff(Z,X/on(X,Y)), State) :- \+ compound(Z), goal_achieved(on(X,Y), State), Z \= X.
 
 %jeśli nie ma żadnych celów, to znaczy, że są spełnione
 goals_achieved([], _). 
@@ -35,11 +36,11 @@ achieves(clear(Y),move(X/on(X,Y),Y,_)).
 requires(move(X,Y/on(X,Y),Z),[clear(X),clear(Z)],[on(X,Y)]).
 requires(move(X/on(X,Y),Y,Z),[clear(X/on(X,Y))],[diff(Z,X/on(X,Y)), clear(Z)]).
 
-inst_action(Action, Conditions, State1, InstAction) :- dupa(Action,InstAction),goals_achieved(Conditions,State1).
+inst_action(Action, Conditions, State1, InstAction) :- change_structures_to_simple_var(Action,InstAction),goals_achieved(Conditions,State1).
 
-dupa(move(X/on(_,_),Y/on(_,_),Z),move(X,Y,Z)).
-dupa(move(X/on(_,_),Y,Z),move(X,Y,Z)).
-dupa(move(X,Y/on(_,_),Z),move(X,Y,Z)).
+change_structures_to_simple_var(move(X/on(_,_),Y/on(_,_),Z),move(X,Y,Z)).
+change_structures_to_simple_var(move(X/on(_,_),Y,Z),move(X,Y,Z)).
+change_structures_to_simple_var(move(X,Y/on(_,_),Z),move(X,Y,Z)).
 
 %Sprawdza czy akcja nie zniszczyła już osiągniętego celu. Każda akcja powoduje usunięcie dwóch elementów stanu, więc sprawdzamy czy nie są na liście celów osiągniętych.
 %Można użyć member ponieważ akcja i cele są w pełni ukonkretnione.
@@ -51,6 +52,7 @@ conc([],L2,L2).
 conc([X|Rest1],L2,[X|Rest2]) :- conc(Rest1,L2,Rest2).
 
 %wariant wywołania procedury plan dla użytkownika, w którym nie musi wyznaczyć celów spełnionych AchievedGoals, ponieważ robi to za niego program 
+%wyznaczamy cele, które są już spełnione w stanie początkowym (czyli jest przecięcie zbiorów InitState i Goals)
 plan(InitState, Goals, Plan, FinalState) :- intersect(InitState,Goals,AchievedGoals), plan(InitState, Goals, AchievedGoals, Plan, FinalState).
 
 plan(State, Goals, AchievedGoals, [], State) :- goals_achieved(Goals, State).
